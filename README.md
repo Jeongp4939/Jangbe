36e5df0df41be7c2d6ef2099175d83800ec7a86a
ChaeSang <limcs1998@naver.com>
2024-04-29 16:33:34 +0900

    test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Jangbe
+
 web service for manage Products
+
+채상 변경 테스트
--- jangbe/app/sales/release/page.tsx
+++ jangbe/app/sales/release/page.tsx
@@ -1,7 +1,7 @@
 export default function Release() {
   return (
     <>
-      <div>Release Route</div>
+      <div>Release Route 채상 변경 테스트</div>
     </>
   );
 }
