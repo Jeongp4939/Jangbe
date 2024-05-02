6553058cac5743e6d9f0da507a2cb15c3e1aa580
ChaeSang <limcs1998@naver.com>
2024-05-02 16:39:22 +0900

    출고관리 더미데이터

--- jangbe/app/sales/release/page.tsx
+++ jangbe/app/sales/release/page.tsx
@@ -1,10 +1,67 @@
-import NavBar from '../../navbar'
+'use client'
+
+import NavBar from '../../navbar';
+import styles from './styles.module.css';
+import { useRouter } from 'next/navigation';
+
+const dummyData = [
+  {
+    no: 1,
+    date: '24.04.18',
+    model: 'HW60',
+    year: 2017,
+    currentLocation: '음성',
+    destination: '광주',
+    salesRepresentative: '홍길동 대리',
+    approvalStatus: 'O'
+  }
+];
 
 export default function Release() {
+  const router = useRouter();
+
+  const handleEnterProducts = () => {
+    router.push('/sales/release_request');
+  };
+
   return (
     <>
-      <NavBar/>
-      <div>Release Route</div>
+      <div>
+        <NavBar />
+        <h1>출고관리</h1>
+        
+        <div className={styles.tableContainer}>
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>no.</th>
+                <th>날짜</th>
+                <th>모델</th>
+                <th>연식</th>
+                <th>현위치</th>
+                <th>도착지</th>
+                <th>영업담당자</th>
+                <th>승인여부</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dummyData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.no}</td>
+                  <td>{item.date}</td>
+                  <td>{item.model}</td>
+                  <td>{item.year}</td>
+                  <td>{item.currentLocation}</td>
+                  <td>{item.destination}</td>
+                  <td>{item.salesRepresentative}</td>
+                  <td>{item.approvalStatus}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <button className={styles.button} onClick={handleEnterProducts}>출고 요청서 작성</button>
+        </div>
+      </div>
     </>
   );
 }
---
+++ jangbe/app/sales/release_request/page.tsx
@@ -0,0 +1,10 @@
+import NavBar from '../../NavBar'
+
+export default function Release_Request() {
+  return (
+    <>
+    <NavBar/>
+      <div>출고 요청서</div>
+    </>
+  );
+}
