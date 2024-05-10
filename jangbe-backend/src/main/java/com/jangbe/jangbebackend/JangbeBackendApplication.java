f4e43d8fa26705fc76ac378eee34e42e25366661
Ho Yoon Jeong <jhy1812@naver.com>
2024-05-10 12:17:47 +0900

    chore: back-end 생성

---
+++ jangbe-backend/src/main/java/com/jangbe/jangbebackend/JangbeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jangbe.jangbebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JangbeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JangbeBackendApplication.class, args);
+	}
+
+}
---
+++ jangbe-backend/src/test/java/com/jangbe/jangbebackend/JangbeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jangbe.jangbebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JangbeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
