b08550698121ab585c9da47c21f835657a005c18
박정훈 <ggongjja0@naver.com>
2024-05-11 09:48:22 +0900

    design: 입력 폼 관련 코드 수정
    + 한 줄에 두개의 입력이 오게 구조 변경

--- jangbe/app/products/enter_products/page.tsx
+++ jangbe/app/products/enter_products/page.tsx
@@ -5,44 +5,52 @@ import styles from "./styles.module.css";
 export default function Enter_Products() {
   return (
     <>
-      <h1>재고 작성 페이지</h1>
       <div className={styles.container}>
+        <h1>재고 작성</h1>
         <form className={styles.form}>
-          <label htmlFor="purchaseDate">매입일자:</label>
-          <input type="date" id="purchaseDate" name="purchaseDate" />
+          <div>
+            <label htmlFor="purchaseDate">매입일자:</label>
+            <input type="date" id="purchaseDate" name="purchaseDate" />
 
-          <label htmlFor="category">구분:</label>
-          <input type="text" id="category" name="category" />
+            <label htmlFor="category">구분:</label>
+            <input type="text" id="category" name="category" />
+          </div>
+          <div>
+            <label htmlFor="model">모델:</label>
+            <input type="text" id="model" name="model" />
 
-          <label htmlFor="model">모델:</label>
-          <input type="text" id="model" name="model" />
+            <label htmlFor="customerName">고객명:</label>
+            <input type="text" id="customerName" name="customerName" />
+          </div>
 
-          <label htmlFor="customerName">고객명:</label>
-          <input type="text" id="customerName" name="customerName" />
+          <div>
+            <label htmlFor="chassisNumber">차대번호:</label>
+            <input type="number" id="chassisNumber" name="chassisNumber" />
 
-          <label htmlFor="chassisNumber">차대번호:</label>
-          <input type="number" id="chassisNumber" name="chassisNumber" />
+            <label htmlFor="year">년식:</label>
+            <input type="number" id="year" name="year" />
+          </div>
+          <div>
+            <label htmlFor="salePrice">판매조정가:</label>
+            <input type="text" id="salePrice" name="salePrice" />
 
-          <label htmlFor="year">년식:</label>
-          <input type="number" id="year" name="year" />
+            <label htmlFor="branch">지점:</label>
+            <select id="branch" name="branch">
+              <option value="음성">음성</option>
+              <option value="광주">광주</option>
+            </select>
+          </div>
 
-          <label htmlFor="salePrice">판매조정가:</label>
-          <input type="text" id="salePrice" name="salePrice" />
+          <div>
+            <label htmlFor="remark">특이사항:</label>
+            <input type="text" id="remark" name="remark" />
 
-          <label htmlFor="branch">지점:</label>
-          <select id="branch" name="branch">
-            <option value="음성">음성</option>
-            <option value="광주">광주</option>
-          </select>
-
-          <label htmlFor="remark">특이사항:</label>
-          <input type="text" id="remark" name="remark" />
-
-          <label htmlFor="stockStatus">입고여부:</label>
-          <select id="stockStatus" name="stockStatus">
-            <option value="입고">입고</option>
-            <option value="출고">출고</option>
-          </select>
+            <label htmlFor="stockStatus">입고여부:</label>
+            <select id="stockStatus" name="stockStatus">
+              <option value="입고">입고</option>
+              <option value="출고">출고</option>
+            </select>
+          </div>
 
           <button className={styles.submitButton} type="submit">
             저장
--- jangbe/app/sales/release_request/page.tsx
+++ jangbe/app/sales/release_request/page.tsx
@@ -6,49 +6,56 @@ export default function Release_Request() {
       <h1>재고 작성 페이지</h1>
       <div className={styles.container}>
         <form className={`${styles.form}`}>
-          <label htmlFor="purchaseDate">날짜:</label>
-          <input type="date" id="purchaseDate" name="purchaseDate" />
+          <div>
+            <label htmlFor="purchaseDate">날짜:</label>
+            <input type="date" id="purchaseDate" name="purchaseDate" />
 
-          <label htmlFor="model">모델:</label>
-          <input type="text" id="model" name="model" />
+            <label htmlFor="model">모델:</label>
+            <input type="text" id="model" name="model" />
+          </div>
+          <div>
+            <label htmlFor="chassisNumber">차대번호:</label>
+            <input type="text" id="chassisNumber" name="chassisNumber" />
 
-          <label htmlFor="chassisNumber">차대번호:</label>
-          <input type="text" id="chassisNumber" name="chassisNumber" />
+            <label htmlFor="currentLocation">현위치:</label>
+            <input type="text" id="currentLocation" name="currentLocation" />
+          </div>
+          <div>
+            <label htmlFor="currentManager">담당자:</label>
+            <input type="text" id="currentManager" name="currentManager" />
 
-          <label htmlFor="currentLocation">현위치:</label>
-          <input type="text" id="currentLocation" name="currentLocation" />
+            <label htmlFor="currentPhone">전화번호:</label>
+            <input type="tel" id="currentPhone" name="currentPhone" />
+          </div>
+          <div>
+            <label htmlFor="destination">도착지:</label>
+            <input type="text" id="destination" name="destination" />
 
-          <label htmlFor="currentManager">담당자:</label>
-          <input type="text" id="currentManager" name="currentManager" />
+            <label htmlFor="destinationManager">담당자:</label>
+            <input
+              type="text"
+              id="destinationManager"
+              name="destinationManager"
+            />
+          </div>
+          <div>
+            <label htmlFor="destinationPhone">전화번호:</label>
+            <input type="tel" id="destinationPhone" name="destinationPhone" />
 
-          <label htmlFor="currentPhone">전화번호:</label>
-          <input type="tel" id="currentPhone" name="currentPhone" />
+            <label htmlFor="remark">기타사항:</label>
+            <input type="text" id="remark" name="remark" />
+          </div>
+          <div>
+            <label htmlFor="approvalStatus">승인여부:</label>
+            <select id="approvalStatus" name="approvalStatus">
+              {/* 재고 승인은 관리자만 가능하게끔 수정 필요함 */}
+              <option value="미승인">미승인</option>
+              <option value="승인">승인</option>
+            </select>
 
-          <label htmlFor="destination">도착지:</label>
-          <input type="text" id="destination" name="destination" />
-
-          <label htmlFor="destinationManager">담당자:</label>
-          <input
-            type="text"
-            id="destinationManager"
-            name="destinationManager"
-          />
-
-          <label htmlFor="destinationPhone">전화번호:</label>
-          <input type="tel" id="destinationPhone" name="destinationPhone" />
-
-          <label htmlFor="remark">기타사항:</label>
-          <input type="text" id="remark" name="remark" />
-
-          <label htmlFor="approvalStatus">승인여부:</label>
-          <select id="approvalStatus" name="approvalStatus">
-            {/* 재고 승인은 관리자만 가능하게끔 수정 필요함 */}
-            <option value="미승인">미승인</option>
-            <option value="승인">승인</option>
-          </select>
-
-          <label htmlFor="dispatchInfo">배차정보:</label>
-          <input type="text" id="dispatchInfo" name="dispatchInfo" />
+            <label htmlFor="dispatchInfo">배차정보:</label>
+            <input type="text" id="dispatchInfo" name="dispatchInfo" />
+          </div>
 
           <button className={styles.submitButton} type="submit">
             저장
