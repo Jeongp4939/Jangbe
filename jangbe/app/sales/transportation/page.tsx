246fcdc1828207a90553d493e9fc28d5dcc1f936
박정훈 <ggongjja0@naver.com>
2024-05-14 10:43:11 +0900

    feat: 배차관리 페이지 작성

--- jangbe/app/sales/transportation/page.tsx
+++ jangbe/app/sales/transportation/page.tsx
@@ -1,8 +1,58 @@
+import styles from "./styles.module.css";
+
+const dummyData = [
+  {
+    no: 1,
+    date: "24.04.18",
+    model: "HW60",
+    year: 2017,
+    currentLocation: "음성",
+    destination: "광주",
+    transportationState: "O",
+    salesRepresentative: "홍길동 대리",
+    approvalStatus: "승인",
+  },
+];
 
 export default function Transportation() {
   return (
     <>
-      <div>Transportation Route</div>
+      <div>
+        <h1>배차정보</h1>
+
+        <div className={styles.tableContainer}>
+          <table className={styles.table}>
+            <thead className={styles.thead}>
+              <tr>
+                <th>no.</th>
+                <th>날짜</th>
+                <th>모델</th>
+                <th>연식</th>
+                <th>현위치</th>
+                <th>도착지</th>
+                <th>배차상황</th>
+                <th>영업담당자</th>
+                <th>승인여부</th>
+              </tr>
+            </thead>
+            <tbody>
+              {dummyData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.no}</td>
+                  <td>{item.date}</td>
+                  <td>{item.model}</td>
+                  <td>{item.year}</td>
+                  <td>{item.currentLocation}</td>
+                  <td>{item.destination}</td>
+                  <td>{item.transportationState}</td>
+                  <td>{item.salesRepresentative}</td>
+                  <td>{item.approvalStatus}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   );
 }
